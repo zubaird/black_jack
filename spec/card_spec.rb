1538ee2d793e356005a98be3dc9a2166ea34489f
Kane Baccigalupi <baccigalupi@gmail.com>
2014-11-02 14:36:03 -0800

    card

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 describe BlackJack::Card do
-  let(:card) { BlackJack::Card.new(:heart, face) }
+  let(:card) { BlackJack::Card.new(suit, face) }
+  let(:suit) { :heart }
   let(:face) { 'J' }
 
   describe 'initialized values' do
     it "reports them publicly" do
-      expect(card.suit).to be == :heart
+      expect(card.suit).to be == suit
       expect(card.face).to be == face
     end
   end
@@ -38,13 +39,48 @@ describe BlackJack::Card do
       end
 
       context "when 'Q'" do
-        # write the let
-        it "should be an array with only 10" # write the test
+        let(:face) { 'Q' }
+
+        it "should be an array with only 10" do
+          expect(card.values).to be == [10]
+        end
       end
 
       context "when 'K'" do
-        # write the let
-        it "should be an array with only 10" # write the test
+        let(:face) { 'Q' }
+
+        it "should be an array with only 10" do
+          expect(card.values).to be == [10]
+        end
+      end
+    end
+  end
+
+  describe '#==' do
+    context "when two cards have different values and suits" do
+      let(:card_1) { BlackJack::Card.new(suit, '3') }
+      let(:card_2) { BlackJack::Card.new(suit, '2') }
+
+      it "should not be ==" do
+        expect(card_1 == card_2).to be false
+      end
+    end
+
+    context "when two cards have the same value, but different suits" do
+      let(:card_1) { BlackJack::Card.new(:heart, face) }
+      let(:card_2) { BlackJack::Card.new(:spade, face) }
+
+      it "should not be ==" do
+        expect(card_1 == card_2).to be false
+      end
+    end
+
+    context "when two cards have same suits, but different values" do
+      let(:card_1) { BlackJack::Card.new(suit, face) }
+      let(:card_2) { BlackJack::Card.new(suit, face) }
+
+      it "should not be ==" do
+        expect(card_1 == card_2).to be true
       end
     end
   end
